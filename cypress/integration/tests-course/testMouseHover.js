8e398908d1f7dfbb202961bfbfdcb77ac1222b46
rdwentmp <maaarek7823@gmail.com>
2021-04-29 14:59:52 +0200

    Test: iterate tables and mouse hover

---
+++ cypress/integration/tests-course/testMouseHover.js
@@ -0,0 +1,12 @@
+// <reference types="Cypress" />
+
+describe('My first test suite', function() {
+        
+    it('My first test case', () => {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        
+        cy.get('div.mouse-hover-content').invoke('show')
+        cy.contains("Top").click()
+        cy.url().should('include', 'top')
+    })
+})
